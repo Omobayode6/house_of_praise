f291ae414dfb33185bf593ea1ab23c9ffc7f9c2e
Omobayode6 <omobayode93@gmail.com>
2024-07-04 18:14:30 +0100

    deploy the app

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     <BrowserRouter>
       <ScrollToTop/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> 
         <Route path="/about" element={<About/>} />
         <Route path="/giving" element={<Giving />} />
         <Route path="/media" element={<Media/>} />
